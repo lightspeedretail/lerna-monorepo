c61f3b7c3911d96159178c1e79625577a15c97fd
Joris de Smedt <joris.desmedt@lightspeedhq.com>
2019-02-28 13:56:55 +0100

    Added a script to see which app has changed

---
+++ scripts/changed.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+FOUND_APP=$(lerna changed | grep $1)
+if [ "$1" = "$FOUND_APP" ]; then
+    echo "App is changed, continuing"
+    exit 0
+else
+    echo "App is not changed, we'll stop here"
+    exit 78
+fi
