15dff45604437fb09ddc968d957e7fe4d96a1446
Joris de Smedt <joris.desmedt@lightspeedhq.com>
2019-02-28 14:31:35 +0100

    added changed action

---
+++ .github/actions/app-changed/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Going to check the changed apps"
+CHANGED_APPS=$(lerna changed)
+echo "Changed apps: $CHANGED_APPS"
+FOUND_APP=$(echo $CHANGED_APPS | grep $1)
+if [ "$1" = "$FOUND_APP" ]; then
+    echo "App is changed, continuing "
+    exit 0
+else
+    echo "App is not changed, we'll stop here"
+    exit 78
+fi
