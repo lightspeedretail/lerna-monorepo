e654611cfc23c1c5e976f304e58fafa094aa74dd
Joris de Smedt <joris.desmedt@lightspeedhq.com>
2019-02-28 14:13:39 +0100

    dd

--- scripts/changed.sh
+++ scripts/changed.sh
@@ -2,7 +2,7 @@
 
 FOUND_APP=$(sh -c "yarn lerna:changed" | grep $1)
 if [ "$1" = "$FOUND_APP" ]; then
-    echo "App is changed, continuing"
+    echo "App is changed, continuing "
     exit 0
 else
     echo "App is not changed, we'll stop here"
